1d3254b57cc82b54758dcfebc519f800edfffa2c
Tyler Peckenpaugh <tyler@ty-pe.com>
2017-12-12 17:47:09 -0500

    version inc

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -23,7 +23,7 @@ declare var window: MyWindow;
   templateUrl: 'build/pages/tabs/tabs.html',
 })
 export class TabsPage {
-  version: string = '0.3.00-beta';
+  version: string = '0.3.1-beta';
 
   @ViewChild('tabs') tabsRef: Tabs;
 
